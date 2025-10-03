1f7bfe04c643b342781899eef54fad6f722a4daf
Finn the Panther <finn@finnthepanther.com>
2025-10-02 17:23:33 -0700

    One more eslint fix

--- services/achievements-listener/.eslintrc.cjs
+++ services/achievements-listener/.eslintrc.cjs
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['expo'],
   settings: {
     'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx']
+      },
       typescript: {
         project: __dirname + '/tsconfig.json',
       },
