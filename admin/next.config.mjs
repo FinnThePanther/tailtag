a9b701197a014a0b7d2621d80ada3244fdc10a20
Finn the Panther <finn@finnthepanther.com>
2026-04-14 19:41:56 -0700

    Website deployment config

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
-      allowedOrigins: ['*'],
+      allowedOrigins: ['admin.playtailtag.com', 'localhost:3000'],
     },
   },
 };
