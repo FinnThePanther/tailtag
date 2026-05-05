a8e5938e690918a330cd9755c70d07b05e730801
Finn the Panther <finn@finnthepanther.com>
2026-05-05 07:27:34 -0700

    Change Beta wording to Prerelease

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -835,7 +835,7 @@ export default function HomeScreen() {
         </View>
 
         <View style={[styles.heroBlock, contentWidthStyle]}>
-          <Text style={styles.badge}>BETA</Text>
+          <Text style={styles.badge}>PRERELEASE</Text>
           <Text style={styles.title}>
             Catch fursuits, grow your collection, and make new furry friends!
           </Text>
