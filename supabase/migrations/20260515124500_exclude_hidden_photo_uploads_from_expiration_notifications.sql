18562f4963b129dadfb1d817c90aed5dcd1bee7b
Finn the Panther <finn@finnthepanther.com>
2026-05-14 20:23:56 -0700

    Address follow-up comments

--- supabase/migrations/20260515124500_exclude_hidden_photo_uploads_from_expiration_notifications.sql
+++ supabase/migrations/20260515124500_exclude_hidden_photo_uploads_from_expiration_notifications.sql
@@ -48,3 +48,7 @@ begin
   );
 end;
 $function$;
+
+revoke execute on function public.expire_pending_catches() from public, anon, authenticated;
+
+grant execute on function public.expire_pending_catches() to service_role;
